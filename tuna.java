d79c545a410362ee73573dd5716f292954cb536c
zachary-le <zachary-le@users.noreply.github.com>
2017-07-14 08:54:22 -0400

    Add files via upload

---
+++ tuna.java
@@ -0,0 +1,11 @@
+class tuna {
+    public static void main (String args[]){
+        double tuna; //double is a number with decimal points
+        tuna = 5.28; //assigns a value to tuna
+        System.out.print("I want ");//without the ln at the end, it makes it stay on the same line. 
+        System.out.print(tuna); //to print out a variable, such as tuna, you don't need quotations. Just put the name. 
+        System.out.println(" movies");//with ln, the next print statement would go to the next line
+        System.out.println("test 1 2 3");
+        
+    }
+}
